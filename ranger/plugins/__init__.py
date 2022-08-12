340186537e56cd6d42815579bdbbd2af96b5d8ee
oeyoews <jyao4783@gmail.com>
2022-08-13 00:04:38 +0800

    feat: support icon

--- ranger/plugins/__init__.py
+++ ranger/plugins/__init__.py
@@ -1,14 +1,3 @@
-"""
-Import the sub-packages
-"""
-
-from .patch import rifle
-from .patch import ueberzug
-from .patch import ui
-from .patch import viewmiller
-from .patch import action
-from .patch import directory
-from .patch import loader
-from .patch import statusbar
-from .patch import ccommands
-from .patch import shutil_generatorized
+#  from .compress import compress
+from .extract import (
+    extract, extract_raw, extract_to_dirs)
